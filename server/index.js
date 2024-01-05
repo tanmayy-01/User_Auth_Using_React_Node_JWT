6c28bdd4bbe60419824d6c4e77f079f6d3b0b151
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-01-05 14:34:24 +0530

    Login is working

--- server/index.js
+++ server/index.js
@@ -9,6 +9,22 @@ app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/Customer");
 
+app.post('/login', (req,res) => {
+    const {email, password} = req.body;
+    CustomerModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            if(user.password == password) {
+                res.json("Success")
+            }else {
+                res.json("The password is incorrect")
+            }
+        }else {
+            res.json("No record existed")
+        }
+    })
+})
+
 app.post('/register', (req, res) => {
     CustomerModel.create(req.body)
     .then(customers => res.json(customers))
